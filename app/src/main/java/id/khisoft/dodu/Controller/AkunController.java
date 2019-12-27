db8322fb99e0e2d9e266f436afa2fea486e6a4e8
Khisby <khisby@gmail.com>
2019-12-27 20:06:18 +0700

    perbaikan tampilan login

--- app/src/main/java/id/khisoft/dodu/Controller/AkunController.java
+++ app/src/main/java/id/khisoft/dodu/Controller/AkunController.java
@@ -4,24 +4,18 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.text.Editable;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import id.khisoft.dodu.home_screen;
 import id.khisoft.dodu.login_screen;
